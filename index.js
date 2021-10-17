c6bdd12adef69ff5c4439adbab35586a89871acd
KindMaple <angelattack9759@gmail.com>
2021-10-18 02:31:16 +0900

    ✨feat: 問題セット

--- index.js
+++ index.js
@@ -73,3 +73,89 @@ function solveJs(problem) {
   }
   return result;
 }
+
+// prettier-ignore
+const PROBLEM_SET = [
+  [
+    0, 0, 0, 0, 0, 0, 6, 0, 0,
+    0, 3, 0, 0, 0, 0, 0, 1, 0,
+    9, 0, 0, 0, 0, 5, 0, 0, 0,
+    2, 0, 0, 3, 1, 0, 0, 0, 0,
+    1, 4, 0, 6, 7, 8, 0, 0, 0,
+    7, 8, 6, 5, 2, 9, 0, 3, 4,
+    5, 6, 7, 0, 9, 3, 0, 0, 1,
+    8, 9, 1, 4, 5, 6, 3, 7, 2,
+    0, 2, 4, 0, 8, 7, 5, 0, 9,
+  ],
+  [
+    1, 0, 0, 0, 0, 8, 0, 4, 0,
+    0, 0, 8, 0, 0, 9, 0, 6, 0,
+    0, 4, 5, 0, 0, 2, 8, 0, 0,
+    8, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 1, 0, 0, 7, 0, 0, 9, 0,
+    0, 0, 0, 0, 0, 0, 0, 0, 7,
+    0, 0, 7, 5, 0, 0, 6, 2, 0,
+    0, 5, 0, 6, 0, 0, 7, 0, 0,
+    0, 2, 0, 3, 0, 0, 0, 0, 9,
+  ],
+  [
+    9, 0, 0, 7, 6, 0, 0, 5, 0,
+    0, 0, 6, 3, 0, 0, 0, 0, 0,
+    0, 1, 0, 9, 0, 0, 0, 0, 4,
+    0, 2, 0, 0, 0, 0, 0, 0, 1,
+    0, 0, 0, 0, 0, 0, 0, 0, 0,
+    7, 0, 0, 0, 0, 0, 0, 2, 0,
+    5, 0, 0, 0, 0, 8, 0, 7, 0,
+    0, 0, 0, 0, 0, 6, 3, 0, 0,
+    0, 4, 0, 0, 3, 1, 0, 0, 8,
+  ],
+  [
+    0, 8, 0, 0, 0, 6, 5, 0, 0,
+    0, 0, 9, 0, 0, 7, 0, 0, 3,
+    5, 0, 0, 3, 0, 2, 0, 6, 0,
+    0, 4, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 5, 0,
+    0, 7, 0, 8, 0, 3, 0, 0, 4,
+    6, 0, 0, 1, 0, 0, 2, 0, 0,
+    0, 0, 3, 4, 0, 0, 0, 7, 0,
+  ]
+]
+
+const wasm = import("../pkg").catch(console.error);
+
+Promise.all([wasm]).then(async function ([{ solve }]) {
+  for (let p in PROBLEM_SET) {
+    let problem = PROBLEM_SET[p];
+    console.log("problem = ", problem);
+
+    let resultWasm;
+    let resultJs;
+    {
+      const solveStartTime = Date.now();
+      resultWasm = solve(problem);
+      const solveEndTime = Date.now();
+      console.log(
+        `solve:wasm\tsolve_elapsed:${solveEndTime - solveStartTime}[ms]`
+      );
+    }
+    {
+      const solveStartTime = Date.now();
+      resultJs = solveJs(problem);
+      const solveEndTime = Date.now();
+      console.log(
+        `solve:js\tsolve_elapsed:${solveEndTime - solveStartTime}[ms]`
+      );
+    }
+
+    for (let i = 0; i < 9; i++) {
+      for (let j = 0; j < 9; j++) {
+        if (resultWasm[9 * i + j] !== resultJs[9 * i + j]) {
+          console.log(resultWasm.slice(9 * i, 9 * i + 9));
+          console.log(resultJs.slice(9 * i, 9 * i + 9));
+          break;
+        }
+      }
+    }
+  }
+});
